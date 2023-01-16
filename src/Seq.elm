8e51aa5911903561fd60daf0dbb440c82eac7d65
Casper Schipper <casper.schipper@gmail.com>
2023-01-16 22:01:48 +0100

    added elm test

--- src/Seq.elm
+++ src/Seq.elm
@@ -1,5 +1,4 @@
 module Seq exposing (..)
-
 import Html exposing (a)
 
 
@@ -12,20 +11,43 @@ type Node a
     | Nil
 
 
+lengthHelper seq acc =
+    case seq () of
+        Nil -> acc
+
+        Cons _ tail -> lengthHelper tail (1 + acc)
+
+length sq =
+    lengthHelper sq 0
+        
+
 toList : Seq a -> List a
 toList sq =
-    case sq () of
-        Nil ->
-            []
+    let 
+        aux seq acc =
+            case seq () of
+                Nil ->
+                    acc |> List.reverse
 
-        Cons x tail ->
-            x :: toList tail
-
+                Cons x tail ->
+                    aux tail (x :: acc)
+    in
+    aux sq [] 
 
 empty : Seq a
 empty () =
     Nil
 
+
+fromListHelper : List a -> Seq a -> Seq a
+fromListHelper lst acc =
+    case lst of
+        [] -> 
+            reverse acc
+
+        x :: xs -> 
+            fromListHelper xs (\() -> Cons x acc)
+            
 fromList : List a -> Seq a
 fromList lst () =
     case lst of
@@ -33,7 +55,7 @@ fromList lst () =
             Nil
 
         x :: xs ->
-            Cons x (fromList xs)
+            Cons x (fromList xs) 
 
 
 repeat : Int -> a -> Seq a
@@ -45,14 +67,82 @@ repeat n x () =
         Cons x (repeat (n - 1) x)
 
 
-map : (a -> b) -> Seq a -> Seq b
-map f sq () =
+mapHelper : (a -> b) -> Seq a -> Seq b -> Seq b
+mapHelper fn sq acc =
     case sq () of
-        Cons x tail ->
-            Cons (f x) (map f tail)
+        Nil -> reverse acc
 
-        Nil ->
-            Nil
+        Cons x xs ->
+            mapHelper fn xs (\() -> Cons (fn x) acc)
+
+map : (a -> b) -> Seq a -> Seq b
+map f sq =
+    mapHelper f sq empty
+    
+
+    
+
+
+andThen : (a -> Seq b) -> Seq a -> Seq b 
+andThen f sq =
+    map f sq |> concat
+
+
+foldl : (acc -> a -> acc) -> acc -> Seq a -> acc
+foldl f acc sq =
+    case sq () of
+        Nil -> acc
+
+        Cons x xs -> 
+            foldl f (f acc x) xs
+
+reverseHelper : Seq a -> Seq a -> Seq a
+reverseHelper seq acc =
+    case seq () of
+        Nil -> 
+            acc
+
+        Cons x xs -> 
+            reverseHelper xs (\() -> Cons x acc)
+
+
+cons : a -> Seq a -> Seq a
+cons x sq () =
+    Cons x sq
+
+
+flip : (a -> b -> c) -> b -> a -> c
+flip f x y =
+    f y x
+
+reverse : Seq a -> Seq a
+reverse sq =
+    foldl (flip cons) empty sq
+
+
+zip : Seq a -> Seq b -> Seq (a,b)
+zip sq1 sq2 () =
+    let
+        helper sq11 sq22 acc () = 
+            case sq11 () of
+                Nil -> acc
+
+                Cons x tail -> 
+                    case sq22 () of
+                        Nil -> 
+                            Nil
+
+                        Cons x2 tail2 ->
+                            helper tail tail2 (Cons (x,x2) (\() -> acc)) ()
+
+    in
+    reverse (helper sq1 sq2 Nil) ()
+    
+    
+
+andMap : Seq (a -> b) -> Seq a -> Seq b
+andMap fsq sq =
+    zip fsq sq |> map (\(f,x) -> f x )
 
 
 take : Int -> Seq a -> Seq a
@@ -84,6 +174,7 @@ filter_map f sq () =
             Nil
 
 
+
 unzip : Seq ( a, b ) -> ( Seq a, Seq b )
 unzip sq =
     ( map Tuple.first sq, map Tuple.second sq )
@@ -113,20 +204,20 @@ append s1 s2 () =
         Cons x tail ->
             Cons x (append tail s2)
 
-cycle_nonempty : (Seq a) -> Seq a
+
+cycle_nonempty : Seq a -> Seq a
 cycle_nonempty xs () =
-  append xs (cycle_nonempty xs) ()
+    append xs (cycle_nonempty xs) ()
 
 
-cycle : Seq a -> Seq a 
+cycle : Seq a -> Seq a
 cycle sq () =
     case sq () of
-        Nil -> Nil
+        Nil ->
+            Nil
 
-        Cons x tail -> Cons x (append tail (cycle_nonempty tail))
-
-
-
+        Cons x tail ->
+            Cons x (append tail (cycle_nonempty tail))
 
 
 concat : Seq (Seq a) -> Seq a
--- src/Sq.elm
+++ src/Sq.elm
@@ -1,72 +1,88 @@
 module Sq exposing (..)
 
-import Random
-import State exposing (State)
+import Dict exposing (Dict)
 import Main exposing (PitchClass(..))
+import Random
 import Seq exposing (Seq)
+import State exposing (State)
+
 
 type CispST
-    = CispST { seed : Random.Seed }
-
+    = CispST
+        { seed : Random.Seed
+        , dict : Dict String Int
+        }
 
 
 initState : Int -> CispST
 initState initialSeed =
-    CispST { seed = Random.initialSeed initialSeed }
+    CispST { seed = Random.initialSeed initialSeed, dict = Dict.empty }
+
 
 
 -- State return, just takes an s and embeds it into a state transformer
 
-type Value 
+
+type Value
     = V Int
     | Compute (State CispST Int)
 
+
 rv : Int -> Int -> Value
 rv a b =
-    let 
-        gen = 
+    let
+        gen =
             Random.int a b
 
-        f : CispST -> (Int,CispST)
+        f : CispST -> ( Int, CispST )
         f (CispST st) =
-            Random.step gen st.seed |> (\(value,newSeed) -> (value, CispST { seed = newSeed })) 
+            Random.step gen st.seed |> (\( value, newSeed ) -> ( value, CispST { st | seed = newSeed } ))
 
         rst : State CispST Int
-        rst =   
+        rst =
             State.advance f
-
     in
-    Compute (rst) 
+    Compute rst
+
+
 toList : CispST -> Seq Value -> List Int
 toList state sq =
-    let 
-        states = 
-            Seq.map (\value ->
-                case value of
-                    V i -> State.state i
-            
-                    Compute st -> st 
-                ) sq
+    let
+        states =
+            Seq.map
+                (\value ->
+                    case value of
+                        V i ->
+                            State.state i
+
+                        Compute st ->
+                            st
+                )
+                sq
     in
-    State.finalValue state (states |> Seq.toList |> State.combine) 
+    State.finalValue state (states |> Seq.toList |> State.combine)
 
 
-myList = 
-    [V 3, V 9, V 2, rv 10 20, rv 100 200, rv 10 200 ] |> Seq.fromList |> Seq.cycle 
+myList =
+    [ V 3, V 9, V 2, rv 10 20, rv 100 200, rv 10 200 ] |> Seq.fromList |> Seq.cycle
+
 
 lstA =
-    [1,2,3,4] |> List.map V |> Seq.fromList 
+    [ 1, 2, 3, 4 ] |> List.map V |> Seq.fromList |> Seq.cycle
+
 
 lstB =
-    [33,22,11] |> List.map V |> Seq.fromList
+    [ 33, 22, 11 ] |> List.map V |> Seq.fromList |> Seq.cycle
+
 
 lstC =
-    [100,200,300,400,500] |> List.map V |> Seq.fromList
+    [ 100, 200, 300, 400, 500 ] |> List.map V |> Seq.fromList |> Seq.cycle
+
 
 combi : List Int
 combi =
-    [lstA,lstB,lstC] |> Seq.fromList |> Seq.transpose |> Seq.concat |> Seq.take 20 |> toList (initState 3)
+    [ lstA, lstB, lstC ] |> Seq.fromList |> Seq.transpose |> Seq.concat |> Seq.take 100 |> toList (initState 3)
+
+
 
 -- An infinite sequence of random number between a and b
-
-
